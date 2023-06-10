a4bb1bce899166cec0bd6b850f968c2ad5884e7f
Travis Cline <travis.cline@gmail.com>
2023-06-09 18:06:21 -0700

    react-frontend: cleanup

--- react-frontend/src/gqlClient.ts
+++ react-frontend/src/gqlClient.ts
@@ -1,4 +1,4 @@
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 import { split, HttpLink } from '@apollo/client';
 import { getMainDefinition } from '@apollo/client/utilities';
 import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
