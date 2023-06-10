2faa8e1c4412021c62a6c932fb5f8e9c20eaa242
Travis Cline <travis.cline@gmail.com>
2023-06-09 17:40:16 -0700

    react-frontend: Pull telemetry until upstream source maps are fixed

--- react-frontend/src/index.tsx
+++ react-frontend/src/index.tsx
@@ -5,10 +5,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import FrontendTracer from './telemetry';
 import { client } from './gqlClient';
-
-FrontendTracer('');
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
